6acbd29e25b3bb99654104938fecb174d5870577
marty-suzuki <s1180183@gmail.com>
2020-03-16 16:20:52 +0900

    Add Computed

---
+++ Unio/Computed.swift
@@ -0,0 +1,31 @@
+//
+//  Computed.swift
+//  Unio
+//
+//  Created by marty-suzuki on 2020/2/19.
+//  Copyright © 2020 tv.abema. All rights reserved.
+//
+
+import Foundation
+
+/// Represents computed property
+public final class Computed<Element> {
+
+    public var value: Element {
+        return getter()
+    }
+
+    private let getter: () -> Element
+
+    public init(getter: @escaping () -> Element) {
+        self.getter = getter
+    }
+
+    public init(capture element: Element) {
+        self.getter = { [element] in element }
+    }
+
+    public init(_ computed: Computed<Element>) {
+        self.getter = computed.getter
+    }
+}
